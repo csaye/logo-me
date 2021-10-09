9bfb4e2efea716318d1513823587245c94f72613
Cooper Saye <coopersaye@gmail.com>
2021-10-09 12:08:15 -0700

    Create title display.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
+  const [title, setTitle] = useState('');
+
   return (
     <div>
+      <h1>LogoMe</h1>
+      <input
+        placeholder="title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <p>{title}</p>
     </div>
   );
 }
