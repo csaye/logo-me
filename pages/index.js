a771d1f0b021633b0a7f14c20e8321c0eb754415
Cooper Saye <coopersaye@gmail.com>
2021-10-09 12:49:46 -0700

    Implement font weight selection.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 
-import { fonts } from '../data/fonts';
+import { fonts, weights } from '../data/fonts';
 
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
   const [title, setTitle] = useState('');
   const [font, setFont] = useState('Roboto');
+  const [weight, setWeight] = useState('400');
 
   return (
     <div>
@@ -16,11 +17,18 @@ export default function Index() {
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
-      <h1 style={{ fontFamily: font }}>{title}</h1>
+      <h1 style={{ fontFamily: font, fontWeight: weight }}>{title}</h1>
       <select value={font} onChange={e => setFont(e.target.value)}>
         {
           fonts.map(font =>
             <option value={font} key={font}>{font}</option>
+          )
+        }
+      </select>
+      <select value={weight} onChange={e => setWeight(e.target.value)}>
+        {
+          weights.map(weight =>
+            <option value={weight} key={weight}>{weight}</option>
           )
         }
       </select>
