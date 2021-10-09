95b0f296042db56bfb02014b2565b92feffc824a
Cooper Saye <coopersaye@gmail.com>
2021-10-09 12:36:00 -0700

    Implement font selection.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 
+import { fonts } from '../data/fonts';
+
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
   const [title, setTitle] = useState('');
+  const [font, setFont] = useState('Roboto');
 
   return (
     <div>
@@ -13,7 +16,14 @@ export default function Index() {
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
-      <p>{title}</p>
+      <h1 style={{ fontFamily: font }}>{title}</h1>
+      <select value={font} onChange={e => setFont(e.target.value)}>
+        {
+          fonts.map(font =>
+            <option value={font} key={font}>{font}</option>
+          )
+        }
+      </select>
     </div>
   );
 }
