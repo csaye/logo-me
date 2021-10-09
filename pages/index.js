a865613e86b77b4e5d4b02a6cbc21475c7bbd5ec
Cooper Saye <coopersaye@gmail.com>
2021-10-09 14:52:17 -0700

    Create logo canvas.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { fonts, weights } from '../data/fonts';
 
 import styles from '../styles/Index.module.css';
 
+const width = 256;
+const height = 256;
+
+let canvas, ctx;
+
 export default function Index() {
+  const canvasRef = useRef();
+
   const [title, setTitle] = useState('');
   const [font, setFont] = useState('Roboto');
   const [weight, setWeight] = useState('400');
 
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
       <h1>LogoMe</h1>
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+      />
       <input
         placeholder="title"
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
-      <h1 style={{ fontFamily: font, fontWeight: weight }}>{title}</h1>
       <select value={font} onChange={e => setFont(e.target.value)}>
         {
           fonts.map(font =>
